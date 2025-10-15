5e7300bc6ca21f7a92b0029b174a6039fa3d3b34
Centik4 <azeh.342d@gmail.com>
2025-10-15 14:22:01 +0300

    added realezation of stack

---
+++ lib_stack/stack.cpp
@@ -0,0 +1,5 @@
+// Copyright 2024 Marina Usova
+
+#include <stdexcept>
+#include "../lib_easy_example/easy_example.h"
+
---
+++ lib_stack/stack.h
@@ -0,0 +1,93 @@
+// Copyright 2024 Marina Usova
+
+#ifndef LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
+#define LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
+
+template<class T>
+class Stack {
+	T* _data;
+	int _size;
+	int _top;
+
+public:
+	Stack();
+	Stack(int size = 20);
+	Stack(const Stack<T>&);
+
+	bool empty() const noexcept;
+	bool full() const noexcept;
+
+	inline int get_size() const noexcept;
+
+	void push(const T&);
+	void pop();
+	void clear();
+
+	T& top();
+	const T& top() const;
+};
+
+template<class T>
+Stack<T>::Stack() : _top(-1), _size(0), _data(nullptr) {}
+
+template<class T>
+Stack<T>::Stack(int size) : _top(-1), _size(size), _data(new T[size]){}
+
+template<class T>
+void Stack<T>::clear() {
+	_size = 0;
+	_top = -1;
+	delete[](*this);
+}
+
+template<class T>
+Stack<T>::Stack(const Stack<T>& A) {
+	clear();
+	_size = A._size;
+	_top = A._top;
+	for (int i = 0; i < _size; i++)
+		_data[i] = A._data[i];
+}
+
+template<class T>
+bool Stack<T>::empty() const noexcept {
+	return (- 1 == _top)
+}
+
+template<class T>
+bool Stack<T>::full() const noexcept {
+	return (_size - 1 == _top);
+}
+
+template<class T>
+inline int Stack<T>::get_size() const noexcept {
+	return _size;
+}
+
+template<class T>
+void Stack<T>::push(const T& A) {
+	if (!full())
+		_data[++_top] = A;
+
+	else
+		throw std::logik_error(" overflow");
+}
+
+template<class T>
+void Stack<T>::pop() {
+	if (!empty())
+		--_top;
+	else
+		throw std::logik_error(" overflow");
+}
+
+template<class T>
+T& Stack<T>::top() {
+	return _data[top];
+}
+
+template<class T>
+const T& Stack<T>::top() const{
+	return _data[top];
+}
+#endif  // LIB_EASY_EXAMPLE_EASY_EXAMPLE_H_
